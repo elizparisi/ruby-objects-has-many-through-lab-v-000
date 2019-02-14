ba7742eb3726acb639cc8e6ee1e959ceb814a0e4
Elizabeth Parisi <parisielizabeth@gmail.com>
2019-02-14 04:46:33 +0000

    Automatically backed up by Learn

--- lib/patient.rb
+++ lib/patient.rb
@@ -17,10 +17,11 @@ class Patient
   end
 
   def appointments
-    Appointment.all.select { |appointment| appointment.patient == self }
+    Appointment.all.select do |appointment| appointment.patient == self
   end
   
-  def doctors
+ def doctors
     appointments.map(&:doctor)
   end
+end
 end
